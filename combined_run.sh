07ebfd8ec8cdf3479c153d22424ed789d887486b
sugamkuchhal <sugamkuchhal@gmail.com>
2025-11-05 08:58:13 +0530

    Update combined_run.sh

--- combined_run.sh
+++ combined_run.sh
@@ -42,5 +42,7 @@ echo "Running: GTT Processor"
 python3 gtt_processor.py --sheet-id "14G8Yinl28F9ZROedyhiH4p5jCz2bcfA2goVB21PVE1s" --sheet-name "DEL_GTT_INS"
 python3 gtt_processor.py --sheet-id "14G8Yinl28F9ZROedyhiH4p5jCz2bcfA2goVB21PVE1s" --sheet-name "INS_GTT_INS"
 python3 gtt_processor.py --sheet-id "14G8Yinl28F9ZROedyhiH4p5jCz2bcfA2goVB21PVE1s" --sheet-name "GTT_INS"
+python3 gtt_processor.py --sheet-id "14G8Yinl28F9ZROedyhiH4p5jCz2bcfA2goVB21PVE1s" --sheet-name "ALTER_GTT_INS"
+python3 gtt_processor.py --sheet-id "14G8Yinl28F9ZROedyhiH4p5jCz2bcfA2goVB21PVE1s" --sheet-name "INS_GTT_INS"
 
 echo "✅ All tasks completed."
