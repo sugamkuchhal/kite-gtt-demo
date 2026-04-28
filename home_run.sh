68ed0d94ad1a9cd4185ba72c33ba82d463cf7a89
sugamkuchhal <sugamkuchhal@gmail.com>
2026-04-28 08:39:02 +0530

    Update home_run.sh

--- home_run.sh
+++ home_run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Running: GTT Processor"
-python3 gtt_processor.py --sheet-id "14G8Yinl28F9ZROedyhiH4p5jCz2bcfA2goVB21PVE1s" --sheet-name "DEL_GTT_INS"
-python3 gtt_processor.py --sheet-id "14G8Yinl28F9ZROedyhiH4p5jCz2bcfA2goVB21PVE1s" --sheet-name "INS_GTT_INS"
-python3 gtt_processor.py --sheet-id "14G8Yinl28F9ZROedyhiH4p5jCz2bcfA2goVB21PVE1s" --sheet-name "GTT_INS"
-python3 gtt_processor.py --sheet-id "14G8Yinl28F9ZROedyhiH4p5jCz2bcfA2goVB21PVE1s" --sheet-name "ALTER_GTT_INS"
-python3 gtt_processor.py --sheet-id "14G8Yinl28F9ZROedyhiH4p5jCz2bcfA2goVB21PVE1s" --sheet-name "INS_GTT_INS"
+python3 gtt_processor.py --ref-sheets "PORTFOLIO" --sheet-name "DEL_GTT_INS"
+python3 gtt_processor.py --ref-sheets "PORTFOLIO" --sheet-name "INS_GTT_INS"
+python3 gtt_processor.py --ref-sheets "PORTFOLIO" --sheet-name "GTT_INS"
+python3 gtt_processor.py --ref-sheets "PORTFOLIO" --sheet-name "ALTER_GTT_INS"
+python3 gtt_processor.py --ref-sheets "PORTFOLIO" --sheet-name "INS_GTT_INS"
 
 echo "✅ All tasks completed."
