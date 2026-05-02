2b8147d0dcb7568eaa208736d782bc6bc4b6772b
sugamkuchhal <sugamkuchhal@gmail.com>
2026-05-02 15:58:50 +0530

    Update combined_holiday_run.sh

--- combined_holiday_run.sh
+++ combined_holiday_run.sh
@@ -5,5 +5,3 @@ python3 fetch_all_gtts.py
 python3 fetch_holdings.py
 python3 date_ext.py
 python3 data_val.py
-python3 ops_sort.py --ref-sheets "PORTFOLIO" --green-tab "Mailing_List" --red-tab "Old_Mailing_List" --yellow-tab "Action_Mailing_List" --loose-update
-python3 algo_tickers_mailer.py --emails "sugamkuchhal@gmail.com"
