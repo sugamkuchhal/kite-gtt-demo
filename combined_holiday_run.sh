17bd59405d07936440102f987fb65f72797f7558
sugamkuchhal <sugamkuchhal@gmail.com>
2026-05-02 15:52:26 +0530

    Update combined_holiday_run.sh

--- combined_holiday_run.sh
+++ combined_holiday_run.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-python3 zerodha_tick_size.py
-bash combined_fetcher.sh
-python3 prepare_feed_date_ext.py
-python3 prepare_feed_data_val.py
-python3 prepare_feed_list.py --ref-sheets "FEED" --source-sheet "SGST_OPEN_LIST" --dest-sheet "SGST_FILTERED_TICKERS"
-python3 prepare_feed_list.py --ref-sheets "FEED" --source-sheet "SUPER_OPEN_LIST" --dest-sheet "SUPER_FILTERED_TICKERS"
-python3 prepare_feed_list.py --ref-sheets "FEED" --source-sheet "TURTLE_OPEN_LIST" --dest-sheet "TURTLE_FILTERED_TICKERS"
 python3 set_field_false.py
 python3 fetch_all_gtts.py
 python3 fetch_holdings.py
