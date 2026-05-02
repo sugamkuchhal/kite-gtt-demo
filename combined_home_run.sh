9c8b3787f67a1fc8bb199ec4f8033d34b1f70b92
sugamkuchhal <sugamkuchhal@gmail.com>
2026-05-02 15:56:29 +0530

    Update combined_home_run.sh

--- combined_home_run.sh
+++ combined_home_run.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
 
-echo "Running: GTT Processor"
 python3 gtt_processor.py --ref-sheets "PORTFOLIO" --sheet-name "DEL_GTT_INS"
 python3 gtt_processor.py --ref-sheets "PORTFOLIO" --sheet-name "INS_GTT_INS"
 python3 gtt_processor.py --ref-sheets "PORTFOLIO" --sheet-name "GTT_INS"
 python3 gtt_processor.py --ref-sheets "PORTFOLIO" --sheet-name "ALTER_GTT_INS"
 python3 gtt_processor.py --ref-sheets "PORTFOLIO" --sheet-name "INS_GTT_INS"
 
-echo "✅ All tasks completed."
+python3 ops_sort.py --ref-sheets "PORTFOLIO" --green-tab "Mailing_List" --red-tab "Old_Mailing_List" --yellow-tab "Action_Mailing_List" --loose-update
+python3 algo_tickers_mailer.py --emails "sugamkuchhal@gmail.com"
