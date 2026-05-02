9efda1d6c3ee2234dcf5e9cfe3ceffcdada9b8d9
sugamkuchhal <sugamkuchhal@gmail.com>
2026-05-02 15:51:05 +0530

    Create combined_feeder.py

---
+++ combined_feeder.py
@@ -0,0 +1,5 @@
+python3 prepare_feed_date_ext.py
+python3 prepare_feed_data_val.py
+python3 prepare_feed_list.py --ref-sheets "FEED" --source-sheet "SGST_OPEN_LIST" --dest-sheet "SGST_FILTERED_TICKERS"
+python3 prepare_feed_list.py --ref-sheets "FEED" --source-sheet "SUPER_OPEN_LIST" --dest-sheet "SUPER_FILTERED_TICKERS"
+python3 prepare_feed_list.py --ref-sheets "FEED" --source-sheet "TURTLE_OPEN_LIST" --dest-sheet "TURTLE_FILTERED_TICKERS"
