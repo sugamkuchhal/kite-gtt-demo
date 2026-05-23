59c1d7f05819beae64a4d837ddadf9d6e5192151
sugamkuchhal <sugamkuchhal@gmail.com>
2026-05-23 16:47:27 +0530

    Update combined_fetcher.sh

--- combined_fetcher.sh
+++ combined_fetcher.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
+# python3 nse_combined_fetcher.py --mode stock --worksheet "NSE_Full_Stock_Data" --max-workers 3 --batch-size 50
 python3 nse_combined_fetcher.py --mode stock --ticker-file nse_stock_list.txt --worksheet "NSE_Stock_Data" --max-workers 3 --batch-size 50
 python3 nse_combined_fetcher.py --mode etf --ticker-file nse_etf_list.txt --worksheet "NSE_ETF_Data" --max-workers 3 --batch-size 50
 python3 nse_data_etl.py
