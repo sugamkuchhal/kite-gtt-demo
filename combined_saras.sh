87cc08156da22fa135ebafd9d0e7a31793e0f99a
sugamkuchhal <sugamkuchhal@gmail.com>
2026-06-05 14:49:26 +0530

    Update combined_saras.sh

--- combined_saras.sh
+++ combined_saras.sh
@@ -1,1 +1,1 @@
-python3 google_sheet_tab_ops.py --sheet-id ss --tab ss --formula-columns R,T --hard-copy-columns A,B --date-cell A4 --date-value 2026-06-05
+python3 google_sheet_tab_ops.py --sheet-id SARAS --tab Sugam --formula-columns R,T --hard-copy-columns A,B --date-cell A4 --date-value 2026-06-05
