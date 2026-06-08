8c65a8834b9ad14955916a4622af12bb4428ddd4
sugamkuchhal <sugamkuchhal@gmail.com>
2026-06-08 20:10:11 +0530

    Add row format copy to SARAS sheet ops

--- combined_saras.sh
+++ combined_saras.sh
@@ -1,10 +1,10 @@
 
-python3 google_sheet_tab_ops.py --sheet-id SARAS --tab Kids --hard-copy-columns A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,S,Y --formula-columns R,T,U,V,W,X,AA,AB,AC
+python3 google_sheet_tab_ops.py --sheet-id SARAS --tab Kids --hard-copy-columns A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,S,Y --formula-columns R,T,U,V,W,X,AA,AB,AC --format-source-row 6 --format-destination-row 5
 
 sleep 65
 
-python3 google_sheet_tab_ops.py --sheet-id SARAS --tab Ankita --hard-copy-columns A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,S,Y --formula-columns R,T,U,V,W,X,AA,AB,AC
+python3 google_sheet_tab_ops.py --sheet-id SARAS --tab Ankita --hard-copy-columns A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,S,Y --formula-columns R,T,U,V,W,X,AA,AB,AC --format-source-row 6 --format-destination-row 5
 
 sleep 65
 
-python3 google_sheet_tab_ops.py --sheet-id SARAS --tab Sugam --hard-copy-columns A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,S,Y --formula-columns R,T,U,V,W,X,AA,AB,AC --date-cell A4
+python3 google_sheet_tab_ops.py --sheet-id SARAS --tab Sugam --hard-copy-columns A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,S,Y --formula-columns R,T,U,V,W,X,AA,AB,AC --format-source-row 6 --format-destination-row 5 --date-cell A4
