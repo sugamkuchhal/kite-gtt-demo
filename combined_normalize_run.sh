47426ffe958cb0d7aeeddf47a400f1fd965bfff6
sugamkuchhal <sugamkuchhal@gmail.com>
2026-07-03 13:35:47 +0530

    Update combined_normalize_run.sh

--- combined_normalize_run.sh
+++ combined_normalize_run.sh
@@ -3,4 +3,4 @@
               python3 fifo_portfolio.py
               python3 fetch_holdings.py 
               bash combined_main_run.sh 
-              bash combined_home_run.sh
+              bash combined_home_run_eod.sh
