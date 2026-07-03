275ea9c8d57c12b96e3af28e0b5e688bd04ebfcb
sugamkuchhal <sugamkuchhal@gmail.com>
2026-07-03 13:33:08 +0530

    Update combined_home_run.sh

--- combined_home_run.sh
+++ combined_home_run.sh
@@ -5,7 +5,6 @@ python3 gtt_processor.py --ref-sheets "PORTFOLIO" --sheet-name "GTT_INS"
 python3 gtt_processor.py --ref-sheets "PORTFOLIO" --sheet-name "ALTER_GTT_INS"
 python3 gtt_processor.py --ref-sheets "PORTFOLIO" --sheet-name "INS_GTT_INS"
 
-python3 ops_sort.py --ref-sheets "PORTFOLIO" --green-tab "Mailing_List" --red-tab "Old_Mailing_List" --yellow-tab "Action_Mailing_List" --loose-update
 python3 algo_tickers_mailer.py --emails "sugam.kuchhal.iimc@gmail.com"
 python3 algo_winners_mailer.py --emails "sugam.kuchhal.iimc@gmail.com,sharma.virat@gmail.com"
 
