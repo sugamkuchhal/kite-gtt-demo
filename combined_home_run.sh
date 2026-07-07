2a19a7da46a0eff7f7c1a3053d6f78e585d21d19
sugamkuchhal <sugamkuchhal@gmail.com>
2026-07-07 22:41:01 +0530

    Update combined_home_run.sh

--- combined_home_run.sh
+++ combined_home_run.sh
@@ -2,6 +2,8 @@
 bash combined_home_run_eod.sh
 
 python3 algo_tickers_mailer.py --emails "sugam.kuchhal.iimc@gmail.com"
+python3 corporate_actions_mailer.py --emails "sugam.kuchhal.iimc@gmail.com"
+python3 dividend_action_mailer.py --emails "sugam.kuchhal.iimc@gmail.com"
 python3 algo_winners_mailer.py --emails "sugam.kuchhal.iimc@gmail.com,sharma.virat@gmail.com"
 
 python3 all_old_gtt_ins_backup.py
