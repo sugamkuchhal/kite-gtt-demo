573d3e65e094bc4ce6deeccd5f04ef2876e3f8f8
sugamkuchhal <sugamkuchhal@gmail.com>
2026-07-14 20:32:51 +0530

    Update combined_fetcher.sh

--- combined_fetcher.sh
+++ combined_fetcher.sh
@@ -5,4 +5,4 @@ python3 nse_combined_fetcher.py --mode stock --ticker-file nse_stock_list.txt --
 python3 nse_combined_fetcher.py --mode etf --ticker-file nse_etf_list.txt --worksheet "NSE_ETF_Data" --max-workers 3 --batch-size 50
 
 python3 nse_data_etl.py
-python3 data_teleporter.py --mode inc
+# python3 data_teleporter.py --mode inc
