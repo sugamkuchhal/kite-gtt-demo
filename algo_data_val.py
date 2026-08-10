313bead05e37e97268cb7eb8b9aab434578d3a8a
sugamkuchhal <sugamkuchhal@gmail.com>
2026-08-10 18:04:45 +0530

    Update algo_data_val.py

--- algo_data_val.py
+++ algo_data_val.py
@@ -5,14 +5,8 @@ from data_val_utils import get_ws, check_gt_threshold
 
 
 def main():
-    sh4, ws4 = get_ws("KWK", "Friday_Identifier")
-    check_gt_threshold(sh4.title, ws4, "F1")
-
     sh5, ws5 = get_ws("PORTFOLIO", "CREDIT_CANDIDATES")
     check_gt_threshold(sh5.title, ws5, "K1")
-
-    sh6, ws6 = get_ws("RTP", "DATE_Identifier")
-    check_gt_threshold(sh6.title, ws6, "F1")
 
     sh7, ws7 = get_ws("HUNDRED", "OPEN_LIST")
     check_gt_threshold(sh7.title, ws7, "F1")
